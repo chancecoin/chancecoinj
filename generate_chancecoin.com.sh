078ef3c897a2430de1ba0465d92e855c625fc5dc
Zack Coburn <chancecoin@chancecoin.com>
2014-05-04 00:11:52 -0500

    update

---
+++ generate_chancecoin.com.sh
@@ -0,0 +1,18 @@
+mv resources/templates resources/templates_temp
+mv resources/templates_chancecoincom.html resources/templates
+
+rm -Rf chancecoin.com
+mkdir chancecoin.com
+cp -r resources/static/* chancecoin.com/
+cp .htaccess chancecoin.com/
+mkdir chancecoin.com/downloads
+wget -O chancecoin.com/index.html http://0.0.0.0:8080/
+wget -O chancecoin.com/participate.html http://0.0.0.0:8080/participate
+wget -O chancecoin.com/technical.html http://0.0.0.0:8080/technical
+wget -O chancecoin.com/casino.html http://0.0.0.0:8080/casino
+wget -O chancecoin.com/wallet.html http://0.0.0.0:8080/wallet
+wget -O chancecoin.com/exchange.html http://0.0.0.0:8080/exchange
+wget -O chancecoin.com/balances.html http://0.0.0.0:8080/balances
+
+mv resources/templates resources/templates_chancecoincom.html
+mv resources/templates_temp resources/templates
