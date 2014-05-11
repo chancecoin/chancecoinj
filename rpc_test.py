e50b271fdf153b73cb10cd4906de3aaf74d4af87
TraderCoderGambler <tradercodergambler@gmail.com>
2014-05-11 14:16:10 -0500

    update

--- rpc_test.py
+++ rpc_test.py
@@ -1,5 +1,3 @@
 import pyjsonrpc
-rpc_client = pyjsonrpc.HttpClient(url = "http://127.0.0.1:54321/chancecoin")
-
-print(rpc_client)
-print(rpc_client.getChancecoinBalance("1BckY64TE6VrjVcGMizYBE7gt22axnq6CM"))
+rpc_client = pyjsonrpc.HttpClient(url = "http://127.0.0.1:54121/chancecoin")
+print(rpc_client.getBalance("1BckY64TE6VrjVcGMizYBE7gt22axnq6CM"))
