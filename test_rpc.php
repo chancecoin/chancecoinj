2cf8acec2f26c3febc279b869bcb6b3a5d24a401
Zack Coburn <chancecoin@chancecoin.com>
2014-05-11 14:49:26 -0500

    update

--- test_rpc.php
+++ test_rpc.php
@@ -94,7 +94,7 @@ class Client
 }
 
 $client = new Client("http://127.0.0.1:54121/chancecoin");
-// $client->authentication("chancecoin", "password");
+$client->authentication("chancecoin", "password");
 // $result = $client->execute('reparse', array());
 
 $result = $client->execute('getBalance', array('1BckY64TE6VrjVcGMizYBE7gt22axnq6CM'));
--- test_rpc.py
+++ test_rpc.py
@@ -1,3 +1,4 @@
 import pyjsonrpc
-rpc_client = pyjsonrpc.HttpClient(url = "http://127.0.0.1:54121/chancecoin")
+rpc_client = pyjsonrpc.HttpClient(url = "http://127.0.0.1:54121/chancecoin", username = "chancecoin", password = "password")
+
 print(rpc_client.getBalance("1BckY64TE6VrjVcGMizYBE7gt22axnq6CM"))
