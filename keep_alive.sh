86d503593eb78bbe0154589ec2ffc868331f7d00
Zack Coburn <chancecoin@chancecoin.com>
2014-07-20 10:44:57 -0500

    update

--- keep_alive.sh
+++ keep_alive.sh
@@ -1,7 +1,8 @@
 ps -ef | grep -v grep | grep Server
 if [ $? -eq 1 ]
 then
-nohup sh /var/www/chancecoin.com/chancecoinj/start_server.sh &
+cd /var/www/chancecoin.com/chancecoinj
+nohup sh start_server.sh &
 else
 echo "Already running"
 fi
