36c7b9102f52ad5fb31b208d98468c68e148cac2
TraderCoderGambler <tradercodergambler@gmail.com>
2014-12-06 17:24:37 -0600

    update

--- src/BalanceUpdater.java
+++ src/BalanceUpdater.java
@@ -44,28 +44,18 @@ public class BalanceUpdater {
 					out.print(attributes.toString());
 					out.close();
 					try {
-						//Runtime.getRuntime().exec("git pull; git commit -am 'auto-update balances'; git push;");
-
 						//Git Commit the change and print out any output and errors in the process
 						Runtime rt = Runtime.getRuntime();
 						String[] commands = {"sh", "git_commit.sh"};
 						Process proc = rt.exec(commands);
-
-						BufferedReader stdInput = new BufferedReader(new 
-						     InputStreamReader(proc.getInputStream()));
-
-						BufferedReader stdError = new BufferedReader(new 
-						     InputStreamReader(proc.getErrorStream()));
-
+						BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+						BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
 						// read the output from the command
-						System.out.println("Here is the standard output of the command:\n");
 						String s = null;
 						while ((s = stdInput.readLine()) != null) {
 						    System.out.println(s);
 						}
-
 						// read any errors from the attempted command
-						System.out.println("Here is the standard error of the command (if any):\n");
 						while ((s = stdError.readLine()) != null) {
 						    System.out.println(s);
 						}
